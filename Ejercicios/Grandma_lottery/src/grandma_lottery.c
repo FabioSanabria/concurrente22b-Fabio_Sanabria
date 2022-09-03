ce10e2e8306a1cf24e1f714ada79d470ddce14d4
f_sanabriav <81923295+FabioSanabria@users.noreply.github.com>
2022-09-02 21:11:51 -0600

    Grandma_lottery

--- Ejercicios/Grandma_lottery/src/grandma_lottery.c
+++ Ejercicios/Grandma_lottery/src/grandma_lottery.c
@@ -1,13 +1,17 @@
+// Copyright 2022 Fabio Sanabria <fabio.sanabria@ucr.ac.cr> CC-BY 4.0
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>  // Time library that helps with the random numbers
 // #include <unistd.h>
 
 // Children returning the lottery numbers
-/*
-    @brief
-*/
+/**
+ * @brief Subroutine that simulates the 2 childs buying
+ * lottery, this method uses rand to the random nums and
+ * calloc to return the number
+ * @return The number as a false memory address
+ */
 void* lottery_grandson(void* data);
 
 
@@ -15,18 +19,24 @@ int main() {
     srand(time(NULL));
     pthread_t childson1;
     pthread_t childson2;
-    int error = pthread_create(&childson1, /*attr*/ NULL, lottery_grandson, /*arg*/ NULL);
-    error = pthread_create(&childson2, /*attr*/ NULL, lottery_grandson, /*arg*/ NULL);
-    if(error == EXIT_SUCCESS) {
+    // Creates 2 threads of childson
+    int error = pthread_create(&childson1, /*attr*/ NULL,
+    lottery_grandson, /*arg*/ NULL);
+    error = pthread_create(&childson2, /*attr*/ NULL,
+    lottery_grandson, /*arg*/ NULL);
+
+    // Grandma asking for the numbers to the childson
+    if (error == EXIT_SUCCESS) {
         printf("Grandma: Give me the numbers \n");
-        size_t* random_number1; 
-        size_t* random_number2; 
+        size_t* random_number1;  // Random number
+        size_t* random_number2;  // Random number
+        // Grandma waiting for the childsons
         pthread_join(childson1, /*value_ptr*/ (void*)&random_number1);
         pthread_join(childson2, /*value_ptr*/ (void*)&random_number2);
+        // The childsons saying the numbers that they have
         printf("%zu\n", *random_number1);
         printf("%zu\n", *random_number2);
-    }
-    else{
+    } else {
         fprintf(stderr, "Error: could not create secondary thread\n");
     }
 
@@ -35,11 +45,13 @@ int main() {
 
 void* lottery_grandson(void* data) {
     (void)data;
-    size_t number_rand = (size_t)rand() % 100;   
+    size_t number_rand = (size_t)rand() % 100;
 
+    // False memory address
     size_t* random_number = calloc(1, sizeof(size_t));
 
     random_number[0] = number_rand;
 
+    // Sending the number like a memory address
     return (void*)random_number;
 }
