fafe628cb5aaede4d54a56fa61ca7640f9eee673
f_sanabriav <81923295+FabioSanabria@users.noreply.github.com>
2022-09-21 15:13:31 -0600

    Archivo main de la tarea
    Este es el main que Jeisson a utilizado en la mayoria de las clases, se haran cambios en el si son necesarios para que el programa funcione lo mejor posible

---
+++ Tareas_Programadas/Tarea2_Goldbach_pthread/src/main.c
@@ -0,0 +1,33 @@
+// Goldbach_pthreads program v1.1 Fabio Sanabria Valerin
+// <fabio.sanabria@ucr.ac.cr>
+// Copyright [2022] <Fabio Andres Sanabria Valerin>
+// Main basado en los main pthreads de Jeisson Hidalgo
+#include <assert.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "goldbach.h"
+
+// procedure main
+int main(int argc, char* argv[]) {
+
+  uint64_t thread_count = sysconf(_SC_NPROCESSORS_ONLN);
+  
+  if (argc == 2) {
+    if (sscanf(argv[1], "%" SCNu64, &thread_count) ==1) {
+    } else {
+      fprintf(stderr, "error : invalid thread count\n");
+    }
+  }
+  array_int64_t goldbach;
+  array_int64_init(&goldbach);
+  goldbach_run(&goldbach, thread_count, argc, argv);
+    // goldbach, argc, argv se pasan los argumentos
+  array_int64_destroy(&goldbach);  // se llama al destructor
+
+  return 0;
+}  // end procedure
