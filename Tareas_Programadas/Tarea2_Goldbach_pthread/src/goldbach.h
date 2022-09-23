3eae92683f6db334fd3751ec333d4bc62c05ca08
f_sanabriav <81923295+FabioSanabria@users.noreply.github.com>
2022-09-23 11:12:42 -0600

    Firma de los struct de registros private y share
    Se agrego la firma de los respectivos struct de private data y shared data, ademas de que se incluyó array_int64.h para que no diera mas un error que daba en el main y en este .h

--- Tareas_Programadas/Tarea2_Goldbach_pthread/src/goldbach.h
+++ Tareas_Programadas/Tarea2_Goldbach_pthread/src/goldbach.h
@@ -5,16 +5,16 @@
 #ifndef GOLDBACH_H
 #define GOLDBACH_H
 
-/**
- * @brief Struct of an array used for store different types of variables
-*/
-typedef struct goldbach goldbach_t;  // "Registro opaco"
+#include "array_int64.h"
 
-/**
- * @brief Constructor
- * @return pointer to a goldbach_t object
-*/
-goldbach_t* goldbach_create(void);  // Implemented in the .c "Registro opaco"
+// Struct de datos privados para los hilos
+// Esto es por asi decirlo la firma del struct
+typedef struct private private_data_t;
+
+
+// Struct de datos compartidos para los hilos
+// Esto es por asi decirlo la firma del struct
+typedef struct shared shared_data_t;
 
 /**
  * @brief Run all the necesary process to generate the goldbach sums 
@@ -24,13 +24,7 @@ goldbach_t* goldbach_create(void);  // Implemented in the .c "Registro opaco"
  * @return An error code, EXIT_SUCCESS if the code run correctly or 
  * EXIT_FAILURE if the code fails in this method
 */
-int goldbach_run(goldbach_t* goldbach, int argc, char* argv[]);
-
-/**
- * @brief Destructor
- * @param goldbach Pointer to a goldbach object
- * @return void
-*/
-void goldbach_destroy(goldbach_t* goldbach);
+void goldbach_run(array_int64_t* goldbach, size_t thread_count,
+ int argc, char* argv[]);
 
 #endif  // GOLDBACH_H
