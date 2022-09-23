e32990f45f3f0e72ae3d378abdc23a0389c0ae56
f_sanabriav <81923295+FabioSanabria@users.noreply.github.com>
2022-09-23 13:45:58 -0600

    Se agregaron los metodos de resize y add de sumas
    Se crearon los metodos de resize de sumas, siguen una estructura muy parecida a la de los demás arrays

--- Tareas_Programadas/Tarea2_Goldbach_pthread/src/array_num64.c
+++ Tareas_Programadas/Tarea2_Goldbach_pthread/src/array_num64.c
@@ -24,3 +24,34 @@ void sumas_value_destroy(array_sum_t *array) {
   free(array->sumas_value);
   array->sumas_value = NULL;
 }
+
+// Resize del array de sumas
+int sumas_value_increase_capacity(array_sum_t *array) {
+  size_t new_capacity = 10 * (array->capacity ? array->capacity : 1);
+  sumas_value_t *new_array =
+  (sumas_value_t *)realloc(array->sumas_value,
+  new_capacity * sizeof(sumas_value_t));
+
+  if (new_array) {
+    array->capacity = new_capacity;
+    array->sumas_value = new_array;
+    return EXIT_SUCCESS;
+  }
+  return EXIT_FAILURE;
+}
+
+// Append de los numeros dentro del array
+int sumas_value_append
+(array_sum_t *array, int64_t num1, int64_t num2, int64_t num3) {
+  int error = EXIT_SUCCESS;
+  if (array->count == array->capacity) {
+    error = sumas_value_increase_capacity(array);
+  }
+  if (error == EXIT_SUCCESS) {
+    array->sumas_value[array->count].num1 = num1;
+    array->sumas_value[array->count].num2 = num2;
+    array->sumas_value[array->count].num3 = num3;
+    array->count++;
+  }
+  return error;
+}
